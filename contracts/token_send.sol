7f7bf0fb35c391e2adb3b965c957a6e1974228a0
ultjeremy <jeremykang@berkeley.edu>
2025-07-16 09:58:28 -0700

    TokenSend - Complete Example

--- contracts/token_send.sol
+++ contracts/token_send.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.7.0 <0.9.0;
 
 contract tokenSend {
@@ -11,7 +10,7 @@ contract tokenSend {
     
     // Function that transferes payment from the caller to a receiver
     function sendToken(address payable receiver) external payable {
-
+        
         // Ensure that the sender sends more than nothing
         require(msg.value > 0, "You have to send something.");
 
